fd3815b7bcfc6b6572d6e8672cc659cd7f314f6d
thane98 <ethan@stonefall.com>
2019-12-29 09:25:02 -0600

    Signed stats; fixed bug with stat saving

--- application/ui/widgets/stats_editor.py
+++ application/ui/widgets/stats_editor.py
@@ -1,5 +1,5 @@
+import struct
 from PySide2.QtWidgets import QSpinBox, QLabel, QFormLayout, QGroupBox
-
 from model.project import Game
 from services import service_locator
 from ui.widgets.property_widget import PropertyWidget
@@ -45,13 +45,20 @@ class StatsEditor (QGroupBox, PropertyWidget):
         ]
 
         labels = self._get_labels_for_project()
-        stat_range = self._get_range_for_project()
-        for i in range(0, len(self.editors)):
+        for i in range(0, 8):
             editor = self.editors[i]
-            editor.setRange(stat_range[0], stat_range[1])
+            editor.setRange(-128, 127)
             layout.addRow(QLabel(labels[i]), editor)
-            editor.valueChanged.connect(lambda: self._on_edit(i, editor.value()))
-        self.setLayout(layout)
+            self.setLayout(layout)
+
+        self.editors[0].valueChanged.connect(lambda: self._on_edit(0, self.editors[0].value()))
+        self.editors[1].valueChanged.connect(lambda: self._on_edit(1, self.editors[1].value()))
+        self.editors[2].valueChanged.connect(lambda: self._on_edit(2, self.editors[2].value()))
+        self.editors[3].valueChanged.connect(lambda: self._on_edit(3, self.editors[3].value()))
+        self.editors[4].valueChanged.connect(lambda: self._on_edit(4, self.editors[4].value()))
+        self.editors[5].valueChanged.connect(lambda: self._on_edit(5, self.editors[5].value()))
+        self.editors[6].valueChanged.connect(lambda: self._on_edit(6, self.editors[6].value()))
+        self.editors[7].valueChanged.connect(lambda: self._on_edit(7, self.editors[7].value()))
 
     @staticmethod
     def _get_labels_for_project():
@@ -62,25 +69,20 @@ class StatsEditor (QGroupBox, PropertyWidget):
         else:
             return EDITOR_LABELS
 
-    @staticmethod
-    def _get_range_for_project():
-        driver = service_locator.locator.get_scoped("Driver")
-        project = driver.project
-        if project.game == Game.FE15.value:
-            return [0, 120]
-        else:
-            return [0, 255]
-
     def _on_edit(self, index, value):
         if self.target:
             buffer = self._get_target_value()
-            buffer[index] = value
+            binary_value = struct.pack("b", value)
+            unsigned_value = int(struct.unpack("B", binary_value)[0])
+            buffer[index] = unsigned_value
 
     def _on_target_changed(self):
         if self.target:
             buffer = self._get_target_value()
-            for i in range(0, len(self.editors)):
-                self.editors[i].setValue(buffer[i])
+            for i in range(0, 8):
+                binary_value = struct.pack("B", buffer[i])
+                value = int(struct.unpack("b", binary_value)[0])
+                self.editors[i].setValue(value)
         else:
             for editor in self.editors:
                 editor.setValue(0)
