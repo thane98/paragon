52986285cee0f449337dc059b920f4310126a781
thane98 <ethan@stonefall.com>
2020-01-06 09:42:36 -0600

    Backend code for adding supports

--- application/services/fe14/supports_service.py
+++ application/services/fe14/supports_service.py
@@ -1,4 +1,4 @@
-from bin_streams import BinArchiveReader
+from bin_streams import BinArchiveReader, BinArchiveWriter
 from services.service_locator import locator
 from ui.fe14_support_editor import FE14SupportEditor
 
@@ -28,6 +28,72 @@ class SupportsService:
         # Support table exists. Read and return.
         reader = self._open_reader_at_table(table_number)
         return self._read_supports_from_table(reader)
+
+    def add_support_between_characters(self, character1, character2, support_type):
+        if not self._has_support_table(character1):
+            self._create_support_table(character1)
+        if not self._has_support_table(character2):
+            self._create_support_table(character2)
+        self._add_support_to_table(character1, character2, support_type)
+        self._add_support_to_table(character2, character1, support_type)
+
+    @staticmethod
+    def _has_support_table(character):
+        return character["Support ID"].value != 0xFFFF
+
+    def _create_support_table(self, character):
+        # First, increment master support table count.
+        master_support_table_address = self._get_master_support_table_address()
+        writer = BinArchiveWriter(self.archive, master_support_table_address)
+        reader = BinArchiveReader(self.archive, master_support_table_address)
+        old_count = reader.read_u32()
+        writer.write_u32(old_count + 1)
+
+        # Next, create a pointer to the new table.
+        destination = self.archive.size()
+        pointer_address = master_support_table_address + old_count * 4 + 4
+        self.archive.allocate(pointer_address, 4, False)
+        self.archive.set_internal_pointer(pointer_address, destination)
+
+        # Generate and assign a support ID for the character
+        support_id = self._find_next_support_id()
+        character["Support ID"].value = support_id
+
+        # Allocate and format the new table.
+        writer.seek(self.archive.size())
+        self.archive.allocate_at_end(4)
+        writer.write_u16(support_id)
+
+    def _add_support_to_table(self, character1, character2, support_type):
+        # Jump to the target support table.
+        master_support_table_address = self._get_master_support_table_address()
+        reader = BinArchiveReader(self.archive, master_support_table_address)
+        target_support_id = character1["Support ID"].value
+        reader.seek(reader.tell() + target_support_id * 4 + 4)
+        reader.seek(reader.read_internal_pointer() + 2)  # Skip the owner id.
+
+        # Update the support count.
+        writer = BinArchiveWriter(reader.tell())
+        old_count = reader.read_u16()
+        writer.write_u16(old_count + 1)
+
+        # Create the new support.
+        new_support_address = writer.tell() + old_count * 0xC
+        self.archive.allocate(new_support_address, 0xC, False)
+        writer.seek(new_support_address)
+        writer.write_u16(character2["ID"].value)
+        writer.write_u32(support_type)
+        writer.write_u32(0)
+
+    @staticmethod
+    def _find_next_support_id() -> int:
+        characters_module = locator.get_scoped("Driver").modules["Characters"]
+        characters = characters_module.entries
+        max_support_id = -1
+        for character in characters:
+            if character["Support ID"].value != 0xFFFF:
+                max_support_id = max(max_support_id, character["Support ID"].value)
+        return max_support_id + 1
 
     def _open_reader_at_table(self, table_number):
         addr = self._get_master_support_table_address() + table_number * 4 + 4
