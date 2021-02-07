d7f49e7dcdb113d0dfdb4b4f42a5a577dbf2a08c
thane98 <ethan@stonefall.com>
2021-02-07 12:12:32 -0600

    Fix SoV icons.

--- paragon/core/services/utils.py
+++ paragon/core/services/utils.py
@@ -11,8 +11,8 @@ def safe_texture_load(fn):
 
 def parse_texture_with_uvs(data, texture, uvs):
     return texture.crop(
-        data.int(uvs, "top_bound"),
         data.int(uvs, "left_bound"),
+        data.int(uvs, "top_bound"),
         data.int(uvs, "width"),
         data.int(uvs, "height"),
     )
