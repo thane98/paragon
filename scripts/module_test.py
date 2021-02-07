346140d47f6762ffc270e2ce14c0c6b8f4875100
thane98 <ethan@stonefall.com>
2021-02-07 15:06:43 -0600

    Add Awakening Presets.bin.lz

--- scripts/module_test.py
+++ scripts/module_test.py
@@ -99,6 +99,13 @@ def test_fe13(gd, rom_root, output_root):
         "combotbl",
         "bs/ComboTbl.bin.lz"
     )
+    basic_test(
+        gd,
+        rom_root,
+        output_root,
+        "combotbl_presets",
+        "bs/Presets.bin.lz"
+    )
     multi_test(
         gd,
         rom_root,
