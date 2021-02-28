2c0ff91cb9075406e1f6ce5363e5ab6e5044ec19
thane98 <ethan@stonefall.com>
2021-02-27 19:53:25 -0800

    FE14 sprites.

--- paragon/core/services/fe14_sprites.py
+++ paragon/core/services/fe14_sprites.py
@@ -1,29 +1,33 @@
-from typing import Optional, Tuple
+import logging
+import os
+from typing import Optional, Tuple, List
 
+from PIL import Image
 from PySide2.QtGui import QPixmap
-
+from paragon import paragon as pgn
 from paragon.core.services.sprites import Sprites
+from paragon.core.textures.texture import Texture
 from paragon.model.relevant_sprite_data import RelevantSpriteData
 
 
 class FE14Sprites(Sprites):
-    def _person_to_jobs(self, pid, person_key) -> Tuple[Optional[str], Optional[str]]:
-        rid = self.gd.multi_open("person", person_key)
-        if rid:
-            return self._parse_jobs(rid, "people", pid)
+    def _person_to_identifier(self, rid) -> Optional[str]:
+        # TODO: Customize avatar?
+        pid = self.gd.key(rid)
+        if pid == "PID_プレイヤー男":
+            return "プレイヤー男1_01"
+        elif pid == "PID_プレイヤー女":
+            return "プレイヤー女1_01"
+
+        aid = self.gd.string(rid, "aid")
+        if aid:
+            return aid[4:]
         else:
-            return None, None
+            job, _ = self._person_to_jobs(rid)
+            return job[4:] if job else None
 
-    def _static_character_to_jobs(self, pid) -> Tuple[Optional[str], Optional[str]]:
-        rid, field_id = self.gd.table("characters")
-        return self._parse_jobs(rid, field_id, pid)
-
-    def _parse_jobs(self, rid, field_id, pid) -> Tuple[Optional[str], Optional[str]]:
-        # Get the character.
-        char_rid = self.gd.list_key_to_rid(rid, field_id, pid)
-        if not char_rid:
-            return None, None
-        job = self.gd.rid(char_rid, "class_1")
+    def _person_to_jobs(self, rid) -> Tuple[Optional[str], Optional[str]]:
+        job = self.gd.rid(rid, "class_1")
         if not job:
             return None, None
         else:
@@ -36,12 +40,97 @@ class FE14Sprites(Sprites):
     def _load(self, char, job, team, fallback_job=None) -> Optional[QPixmap]:
         # Try to load the unique sprite
         try:
-            pass
+            sprite_filename = team + "0.bch.lz"
+
+            # Check for a unique sprite.
+            path = os.path.join("unit", "Unique", f"{job}_{char}")
+            anime_path = os.path.join(path, "anime.bin")
+            if self.gd.file_exists(anime_path, False):
+                # Found a unique sprite. Load it!
+                image_path = os.path.join(path, sprite_filename)
+                return self._load_unique_sprite(image_path)
+
+            # Extract data for building the sprite from its components.
+            body_path = os.path.join("unit", "Body", job)
+            anime_path = os.path.join(body_path, "anime.bin")
+            rid = self.gd.multi_open("sprite_data", anime_path)
+            sprite_data = self.get_all_sprite_data(rid)
+
+            # Load by stitching together body and head sprites.
+            body_filename = os.path.join(body_path, sprite_filename)
+            head_path = os.path.join("unit", "Head", char)
+            head_filename = os.path.join(head_path, sprite_filename)
+            return self._load_standard_sprite(sprite_data, body_filename, head_filename)
         except:
-            pass
+            logging.exception("Failed to load sprite.")
+        return None
 
-    def _load_unique_sprite(self, data: RelevantSpriteData, raw_sprite: bytes) -> Optional[QPixmap]:
-        pass
+    def is_vallite(self, person):
+        army = self.gd.rid(person, "army")
+        return bool(army and self.gd.key(army) == "BID_透魔王国軍")
 
-    def _load_standard_sprite(self, data: RelevantSpriteData, raw_body: bytes, raw_head: bytes) -> Optional[QPixmap]:
-        pass
+    def get_all_sprite_data(self, rid) -> List[List[RelevantSpriteData]]:
+        animations = self.gd.items(rid, "animations")
+        res = []
+        for rid in animations:
+            if not self.gd.bool(rid, "is_used"):
+                continue
+            else:
+                frames = []
+                frame_count = self.gd.int(rid, "frame_count")
+                for i in range(0, frame_count):
+                    frame = self.gd.list_get(rid, "frames", i)
+                    frames.append(self.extract_relevant_sprite_data(frame))
+                res.append(frames)
+        return res
+
+    def extract_relevant_sprite_data(self, rid) -> RelevantSpriteData:
+        return RelevantSpriteData(
+            body_offset_x=self.gd.int(rid, "body_draw_offset_x"),
+            body_offset_y=self.gd.int(rid, "body_draw_offset_y"),
+            body_width=self.gd.int(rid, "body_width"),
+            body_height=self.gd.int(rid, "body_height"),
+            body_source_x=self.gd.int(rid, "body_source_position_x"),
+            body_source_y=self.gd.int(rid, "body_source_position_y"),
+            head_offset_x=self.gd.int(rid, "head_draw_offset_x"),
+            head_offset_y=self.gd.int(rid, "head_draw_offset_y"),
+            head_width=self.gd.int(rid, "head_width"),
+            head_height=self.gd.int(rid, "head_height"),
+            head_source_x=self.gd.int(rid, "head_source_position_x"),
+            head_source_y=self.gd.int(rid, "head_source_position_y"),
+        )
+
+    def _load_unique_sprite(self, path: str) -> Optional[QPixmap]:
+        image = self.gd.read_bch_textures(path)
+        image = Texture.from_core_texture(list(image.values())[0])
+        frame = image.crop(32, 0, 64, 32)
+        raw = pgn.increase_alpha(frame.pixel_data)
+        return Image.frombytes("RGBA", (32, 32), raw, "raw", "RGBA").toqpixmap()
+
+    def _load_standard_sprite(
+        self,
+        data: List[List[RelevantSpriteData]],
+        body_path: str,
+        head_path: str
+    ) -> Optional[QPixmap]:
+        body_image = self.gd.read_bch_textures(body_path)
+        body_image = Texture.from_core_texture(list(body_image.values())[0])
+        body_image = body_image.to_pillow_image().rotate(90, expand=True)
+        head_image = self.gd.read_bch_textures(head_path)
+        head_image = Texture.from_core_texture(list(head_image.values())[0])
+        head_image = head_image.to_pillow_image().rotate(90, expand=True)
+
+        head_tmp = Image.new("RGBA", (body_image.width, body_image.height), (0, 0, 0, 0))
+
+        for frame in data[0]:
+            head_paste_x = frame.body_source_x + frame.head_offset_x
+            head_paste_y = frame.body_source_y + frame.head_offset_y
+            left = frame.head_source_x
+            upper = frame.head_source_y
+            right = left + frame.head_width
+            lower = upper + frame.head_height
+            section = head_image.crop((left, upper, right, lower))
+            head_tmp.paste(section, (head_paste_x, head_paste_y))
+        raw = pgn.merge_images_and_increase_alpha(head_tmp.tobytes(), body_image.tobytes())
+        image = Image.frombytes("RGBA", (body_image.width, body_image.height), raw, "raw", "RGBA")
+        return image.crop((32, 0, 64, 32)).toqpixmap()
--- paragon/core/services/sprites.py
+++ paragon/core/services/sprites.py
@@ -21,18 +21,18 @@ class Sprites:
         try:
             team = self.gd.int(spawn, "team")
             pid = self.gd.string(spawn, "pid")
+            person = self._to_character(pid, person_key)
+            if person and self.is_vallite(person):
+                team = "紫"
             job, fallback = self._get_jobs(pid, person_key)
             if not job:
                 return self.default(team)
             else:
-                pid = pid[4:] if pid and pid.startswith("PID_") else pid
-                job = job[4:] if job.startswith("JID_") else job
-                fallback = (
-                    fallback[4:]
-                    if fallback and fallback.startswith("JID_")
-                    else fallback
-                )
-                return self.load(pid, job, team, fallback_job=fallback)
+                char = self._person_to_identifier(person)
+                job = job.replace("JID_", "")
+                if fallback:
+                    fallback = fallback.replace("JID_", "")
+                return self.load(char, job, team, fallback_job=fallback)
         except:
             logging.exception("Failed to read sprite from spawn.")
             return self.default(team)
@@ -54,18 +54,23 @@ class Sprites:
             return self.default(team)
 
     def _get_jobs(self, pid, person_key=None) -> Tuple[Optional[str], Optional[str]]:
-        job = None
-        fallback = None
-        if person_key:
-            job, fallback = self._person_to_jobs(pid, person_key)
-        if not job:
-            job, fallback = self._static_character_to_jobs(pid)
-        return job, fallback
+        if person := self._to_character(pid, person_key):
+            return self._person_to_jobs(person)
+        else:
+            return None, None
 
-    def _person_to_jobs(self, pid, person_key) -> Tuple[Optional[str], Optional[str]]:
+    def _to_character(self, pid, person_key=None) -> Optional[int]:
+        if person_key:
+            rid = self.gd.multi_open("person", person_key)
+            if rid:
+                if char_rid := self.gd.list_key_to_rid(rid, "people", pid):
+                    return char_rid
+        return self.gd.key_to_rid("characters", pid)
+
+    def _person_to_identifier(self, rid) -> Optional[str]:
         raise NotImplementedError
 
-    def _static_character_to_jobs(self, pid) -> Tuple[Optional[str], Optional[str]]:
+    def _person_to_jobs(self, rid) -> Tuple[Optional[str], Optional[str]]:
         raise NotImplementedError
 
     def _load(self, char, job, team, fallback_job=None) -> Optional[QPixmap]:
@@ -80,3 +85,6 @@ class Sprites:
             return self.team_names[team_number]
         else:
             return None
+
+    def is_vallite(self, person):
+        return False
