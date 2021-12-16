ce656d0fc60db1251a283d5788235eb5cab929d6
thane98 <ethan@stonefall.com>
2021-12-15 19:58:41 -0600

    Update github actions

--- package.bat
+++ package.bat
@@ -1,1 +1,8 @@
+call venv\Scripts\activate.bat
+maturin develop --release
 pyinstaller -n paragon -i paragon.ico paragon.spec
+copy paragon.ico dist
+copy LICENSE.txt dist
+xcopy Data dist\Data\ /s /e /y
+xcopy resources dist\resources\ /s /e /y
+xcopy third-party-licenses dist\third-party-licenses\ /s /e /y
