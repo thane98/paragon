0a4d96ee3ce3030efe2d914a293636389c36dcf6
thane98 <>
2025-02-28 20:55:20 -0800

    Version bump

--- paragon/ui/main.py
+++ paragon/ui/main.py
@@ -15,7 +15,7 @@ logging.basicConfig(
         logging.StreamHandler(sys.stdout),
     ]
 )
-logging.info("Paragon Alpha 2-3-5")
+logging.info("Paragon Alpha 2-3-6")
 
 try:
     from PySide6.QtWidgets import QApplication
